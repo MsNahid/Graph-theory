d7f73d0fe355f9ac251d966e6f42263dc0626a9d
MsNahid <m.nahidzaman@gmail.com>
2021-02-01 07:08:36 +0600

    implemented bfs

---
+++ BFS/bfs.py
@@ -0,0 +1,63 @@
+from collections import defaultdict
+
+class Graph:
+
+    def __init__(self):
+        
+        self.graph = defaultdict(list)
+        self.numNodes = None
+        self.numEdges = None
+
+    def buildGraph(self):
+        self.numNodes = int(input("Number of nodes: "))
+        self.numEdges = int(input("Number of edges: "))
+        
+        for i in range(0, self.numEdges):
+            u, v = [int(x) for x in input("Enter two value: ").split()]
+            self.graph[u].append(v)
+            self.graph[v].append(u)
+        
+    def bfs(self, source):
+
+        visited = [False] * (self.numNodes + 1)
+        # print(max(self.graph))
+        distances = [None] * (self.numNodes + 1)
+        
+        queue = []
+        queue.append(source)
+        visited[source] = True
+        distances[source] = 0
+
+
+        while queue:
+
+            u = queue.pop(0)
+            print(u, end = " ")
+
+            for v in self.graph[u]:
+                if visited[v] == False:
+                    queue.append(v)
+                    visited[v] = True
+                    distances[v] = distances[u] + 1
+        
+        print("\nSource to destinations distance.\n")
+        for i in range(1, self.numNodes + 1):
+            print("{} -----> {} = {}".format(source, i, distances[i]))
+
+
+# Driver Code
+
+g = Graph()
+g.buildGraph()
+source = int(input("Source: "))
+print("Breath First Traversal (starting from {})".format(source))
+g.bfs(source)
+
+
+
+
+        
+
+
+    
+
